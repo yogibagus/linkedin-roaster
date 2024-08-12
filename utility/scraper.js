57ea794b835e71ee5a4ea9e8fbbb96a6785c4f9e
yogibagus <yogibaguskarunia@gmail.com>
2024-08-12 19:17:23 +0700

    change user agent

--- utility/scraper.js
+++ utility/scraper.js
@@ -20,7 +20,7 @@ async function getProfileLinkedIn(profileUrl) {
         const { data } = await axios.get(profileUrl, {
             headers: {
                 'Cookie': `li_at=${liAtCookie}`,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
             }
         });
         const $ = cheerio.load(data);
